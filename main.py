ff6a9540f9b373788f73ff59f1b5f292be09b4f5
Leo <523084685@qq.com>
2023-09-20 15:41:36 +0800

    package

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMenu
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMenu, QSystemTrayIcon,QAction
 from PyQt5 import QtCore
+from PyQt5.QtGui import QIcon
 import sys, random
 import data
+import filepath
+
+icon_path = filepath.resource_path("icon.png")
 
 class UI(QWidget):
     def __init__(self):
@@ -15,6 +19,26 @@ class UI(QWidget):
         self.label = QLabel('Hotspot board.', self, styleSheet='color: yellow; font-size: 18px; font-weight: bold;')
         self.label.setFixedWidth(self.width() - 20)
         self.label.move(10, 10)
+        icon = QIcon(icon_path)
+        self.setWindowIcon(icon)
+        
+        # 设置系统托盘
+        self.tray_icon = QSystemTrayIcon(self)
+        self.tray_icon.setIcon(QIcon(icon_path))
+        # 创建托盘菜单
+        self.tray_menu = QMenu()
+        self.show_action = QAction("Show", self)
+        self.quit_action = QAction("Exit", self)
+        self.tray_menu.addAction(self.show_action)
+        self.tray_menu.addAction(self.quit_action)
+        # 将托盘菜单设置给托盘图标
+        self.tray_icon.setContextMenu(self.tray_menu)
+        # 连接信号槽
+        self.show_action.triggered.connect(self.show)
+        self.quit_action.triggered.connect(self.close)
+         # 显示托盘图标
+        self.tray_icon.show()
+        
         # 定时刷新
         self.refresh()
         self.timer = QtCore.QTimer()
@@ -28,6 +52,16 @@ class UI(QWidget):
         # self.imglabel.setPixmap(icon)
         # self.imglabel.move(0, 0)
     
+    # 关闭事件最小化到托盘
+    def closeEvent(self, event):
+        event.ignore()
+        self.hide()
+        self.tray_icon.showMessage(
+            '热点面板',
+            '已最小化到托盘',
+            QSystemTrayIcon.Information,
+            2000
+        )
     
     # 鼠标拖拽效果
     def mousePressEvent(self, event):
@@ -49,7 +83,7 @@ class UI(QWidget):
     def refresh(self):
         # usdcnh = data.getUSDCNH()
         ss, usdcnh = data.getAll()
-        self.label.setText(f'USDCNH: {usdcnh[2]}\nSS: {ss[2]}');
+        self.label.setText(f'USDCNH: {usdcnh[2]}/{usdcnh[3]}\nSS: {ss[2]}/{round(ss[4], 2)}');
         
     # 右键菜单
     def contextMenuEvent(self, event):
