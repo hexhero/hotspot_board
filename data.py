79f1ca417c0016e35d1dcca1be1996517d24a1e2
Leo <523084685@qq.com>
2023-09-21 19:14:10 +0800

    123123

--- data.py
+++ data.py
@@ -3,65 +3,54 @@
 
 '''
 数据来源：https://wallstreetcn.com/
+格式
+[
+    ['a','b','c']
+]
 '''
 
 import requests
 
-def getAll():
-    
+
+def getData():
+
     url = "https://api-ddc-wscn.awtmt.com/market/real?fields=prod_name%2Cpreclose_px%2Clast_px%2Cpx_change%2Cpx_change_rate%2Cprice_precision&prod_code=000001.SS%2CDXY.OTC%2CUS10YR.OTC%2CUSDCNH.OTC%2C399001.SZ%2C399006.SZ"
 
     payload = {}
     headers = {
-    'Accept': '*/*',
-    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive',
-    'DNT': '1',
-    'Origin': 'https://wallstreetcn.com',
-    'Pragma': 'no-cache',
-    'Referer': 'https://wallstreetcn.com/',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'cross-site',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.81',
-    'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Microsoft Edge";v="116"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"'
+        'Accept': '*/*',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'DNT': '1',
+        'Origin': 'https://wallstreetcn.com',
+        'Pragma': 'no-cache',
+        'Referer': 'https://wallstreetcn.com/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'cross-site',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.81',
+        'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Microsoft Edge";v="116"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"'
     }
 
     response = requests.request("GET", url, headers=headers, data=payload)
     data = response.json()
     data = data['data']['snapshot']
     print(data)
-    return data['000001.SS'], data['USDCNH.OTC']
-    
-
-
-def getUSDCNH():
-
-    url = "https://api-ddc-wscn.awtmt.com/market/kline?prod_code=USDCNH.OTC&tick_count=1&period_type=300&adjust_price_type=forward&fields=tick_at,open_px,close_px,high_px,low_px,turnover_volume,turnover_value,average_px,px_change,px_change_rate,avg_px,ma2"
-    payload = {}
-    headers = {
-    'Accept': '*/*',
-    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive',
-    'DNT': '1',
-    'Origin': 'https://wallstreetcn.com',
-    'Pragma': 'no-cache',
-    'Referer': 'https://wallstreetcn.com/',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'cross-site',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.81',
-    'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Microsoft Edge";v="116"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"'
-    }
-    response = requests.request("GET", url, headers=headers, data=payload)
-    data = response.json()
-    val = data['data']['candle']['USDCNH.OTC']['lines'][0][1]
-    print('USDCNH:', val)
-    return val
-
+    rst = [
+        [
+            'USDCNH',
+            str(data['USDCNH.OTC'][2]),
+            '/',
+            str(data['USDCNH.OTC'][3])
+        ],
+        [
+            'SS',
+            str(data['000001.SS'][2]),
+            '/',
+            str(round(data['000001.SS'][4], 2))
+        ]
+    ]
+    return rst
--- main.py
+++ main.py
@@ -6,30 +6,54 @@
 Python版本: 3.x
 '''
 
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMenu, QSystemTrayIcon,QAction
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMenu, QSystemTrayIcon, QAction, QHBoxLayout, QVBoxLayout
 from PyQt5 import QtCore
-from PyQt5.QtGui import QIcon
-import sys, random
-import data
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import Qt
+import sys
+import data as ddd
 import filepath
 
 icon_path = filepath.resource_path("icon.png")
 
 class UI(QWidget):
-    def __init__(self):
+    
+    themes = {
+        'spring': ['#ebeee8', '#d2af9d', '#ea5514', '#3271ae'],
+        'yushui': ['#f9d3e3', '#beb1aa', '#e5a84b', '#c0d695']
+    }
+    
+    def __init__(self, theme='spring'):
         super().__init__()
+        self.theme = UI.themes.get(theme,'spring')
         self.initUI()
-        
+
     def initUI(self):
-        self.setWindowFlags(self.windowFlags() | QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
+        # 设置全局字体
+        font = QFont()
+        font.setFamily('Microsoft YaHei UI')
+        self.setFont(font)
+        # 设置窗口样式
+        self.setWindowFlags(self.windowFlags(
+        ) | QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-        self.setGeometry(120, 120, 800, 300) # x, y, w, h
-        self.label = QLabel('Hotspot board.', self, styleSheet='color: yellow; font-size: 18px; font-weight: bold;')
-        self.label.setFixedWidth(self.width() - 20)
-        self.label.move(10, 10)
+        self.setGeometry(120, 120, 300, 300)  # x, y, w, h
         icon = QIcon(icon_path)
         self.setWindowIcon(icon)
-        
+        # 初始化布局
+        self.init_layout()
+        # 初始化系统托盘
+        self.init_tray()
+        # 初始化任务
+        self.init_task()
+
+    def init_task(self):
+        # 定时刷新
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.refresh_ui)
+        self.timer.start(1000 * 60)
+
+    def init_tray(self):
         # 设置系统托盘
         self.tray_icon = QSystemTrayIcon(self)
         self.tray_icon.setIcon(QIcon(icon_path))
@@ -44,22 +68,27 @@ class UI(QWidget):
         # 连接信号槽
         self.show_action.triggered.connect(self.show)
         self.quit_action.triggered.connect(self.quit_application)
-         # 显示托盘图标
+        # 显示托盘图标
         self.tray_icon.show()
-        
-        # 显示图片
-        # self.imglabel = QLabel(self)
-        # icon = QPixmap('img.png')
-        # icon = icon.scaled(100, 100)
-        # self.imglabel.setPixmap(icon)
-        # self.imglabel.move(0, 0)
-    
-        # 定时刷新
-        self.refresh()
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.refresh)
-        self.timer.start(1000 * 60)
-        
+
+    def init_layout(self):
+        self.refresh_data()
+        self.labels = []
+        wrap = QVBoxLayout()
+        wrap.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+        for d1 in self.data:
+            line = QHBoxLayout()
+            line.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+            subLabel = []
+            for i, d2 in enumerate(d1):
+                clr = self.theme[i] if i < len(self.theme) else '#green'
+                label = QLabel(d2, self, styleSheet=f'color: {clr}; font-size: 14px; font-weight: bold;')
+                line.addWidget(label)
+                subLabel.append(label)
+            wrap.addLayout(line)
+            self.labels.append(subLabel)
+        self.setLayout(wrap)
+
     # 关闭事件最小化到托盘
     def closeEvent(self, event):
         event.ignore()
@@ -70,11 +99,11 @@ class UI(QWidget):
             QSystemTrayIcon.Information,
             2000
         )
-    
+
     def quit_application(self):
         self.tray_icon.hide()
         QApplication.quit()
-    
+
     # 鼠标拖拽效果
     def mousePressEvent(self, event):
         if event.button() == QtCore.Qt.LeftButton:
@@ -92,29 +121,34 @@ class UI(QWidget):
             event.accept()
     
     # 刷新数据
-    def refresh(self):
-        # usdcnh = data.getUSDCNH()
-        ss, usdcnh = data.getAll()
-        self.label.setText(f'USDCNH: {usdcnh[2]}/{usdcnh[3]}\nSS: {ss[2]}/{round(ss[4], 2)}')
-        # self.showMessage('热点面板', f'USDCNH: {usdcnh[2]}/{usdcnh[3]}\nSS: {ss[2]}/{round(ss[4], 2)}', QSystemTrayIcon.Information)
-        
+    def refresh_data(self):
+        self.data = ddd.getData()
+
+    def refresh_ui(self):
+        self.refresh_data()
+        for i,v in enumerate(self.data):
+            labels = self.labels[i]
+            for i2,v2 in enumerate(v):
+                labels[i2].setText(str(v2))
+    
     # 显示系统消息
     # def showMessage(self, title, message, icon):
     #     if icon is None: icon = QSystemTrayIcon.Information
     #     self.tray_icon.showMessage(title, message, icon, 2000)
-        
+
     # 右键菜单
     def contextMenuEvent(self, event):
         cmenu = QMenu(self)
         exitAct = cmenu.addAction("Exit")
         action = cmenu.exec_(self.mapToGlobal(event.pos()))
         if action == exitAct:
-            self.timer.stop()
+            # self.timer.stop()
             self.quit_application()
-            
+
+
 if __name__ == '__main__':
     print(__doc__)
     app = QApplication(sys.argv)
-    ui = UI()
+    ui = UI(theme='yushui')
     ui.show()
     sys.exit(app.exec_())
